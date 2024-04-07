96355f4e78b1698d9cdaa102534927729e79d6ab
mtmattqq <mtmattqq@gmail.com>
2024-04-07 21:39:57 +0800

    Finished

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::{
+    env::args, fs, path::Path
+};
+use colored::Colorize;
+
+fn main() {
+    let args: Vec<String> = args().collect();
+    if args.len() == 1 {
+        println!("Too few argument: missing folder name");
+        return;
+    }
+    else if args.len() > 2 {
+        println!("Too many argument");
+    }
+
+    let folder = Path::new(&args[1]);
+
+    match find_all(&folder) {
+        Ok(_) => println!("Success"),
+        Err(e) => println!("Error: {}", e),
+    };
+}
+
+fn find_all(folder: &Path) -> Result<(), String> {
+    if !folder.is_dir() {
+        return Err("The path is not a diractory".into());
+    }
+    let mut dir_stack: Vec<Box<Path>> = Vec::new();
+    let mut depth_stack: Vec<i32> = Vec::new();
+    dir_stack.push(folder.into());
+    depth_stack.push(0);
+    let color = ["-".bright_blue(), "-".red(), "-".green()];
+
+    while dir_stack.len() > 0 {
+        let file_name = dir_stack.last().clone().unwrap();
+        let depth = depth_stack.last().unwrap().clone();
+        if depth > 0 { print!("{}", "|".yellow());}
+        for i in 0..depth {
+            let len = color.len() as i32;
+            for _ in 0..4 {
+                print!("{}", color[(i % len) as usize]);
+            } 
+        }
+        if depth > 0 { print!(" ");}
+        depth_stack.pop();
+        println!("{}", file_name
+            .to_str().unwrap()
+            .split('/')
+            .last().unwrap());
+        
+        let dir = dir_stack.last().unwrap();
+        let dir = fs::read_dir(dir);
+        dir_stack.pop();
+        let dir = match dir {
+            Ok(d) => d,
+            Err(_) => continue,
+        };
+
+        for entry in dir {
+            let entry = match entry {
+                Ok(e) => e,
+                Err(_) => break,
+            };
+            let path = entry.path();
+            dir_stack.push(path.into());
+            depth_stack.push(depth + 1);
+        }
+    }
+    Ok(())
+}
